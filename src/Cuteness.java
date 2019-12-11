54be9f389392ef03ad9ff44b01be07013053c433
ilabstaff <ilabstaff@172.16.116.68>
2019-12-10 19:29:35 -0800

    Houses

---
+++ src/Cuteness.java
@@ -0,0 +1,7 @@
+
+public class Cuteness {
+public static void main(String[] args) {
+	JFrame frame = new JFrame;
+	JButton 
+}
+}
--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,101 @@
 package _01_houses;
 
+import java.lang.reflect.Method;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	
+	public static void main(String[] args) {
+	Robot rob = new Robot();
+
+	
+	rob.moveTo(150, 550);
+	
+	
+	for (int i = 0; i < 1; i++) {
+	
+	rob.penDown();
+	rob.setSpeed(1000000000);
+	rob.move(100);
+	rob.turn(90);
+	rob.move(65);
+	rob.turn(90);
+	rob.move(100);	
+	rob.turn(-90);
+	rob.setPenColor(0,255,0);
+	rob.move(20);
+	rob.setPenColor(0,0,0);
+	rob.turn(-90);
+	}
+	
+	for (int i = 0; i < 1; i++) {
+	rob.move(100);
+	rob.turn(45);
+	rob.move(65);
+	rob.turn(90);
+	rob.move(65);
+	rob.turn(45);
+	rob.move(100);
+	rob.turn(-90);
+	rob.setPenColor(0,255,0);
+	rob.move(20);
+	rob.setPenColor(0,0,0);
+	rob.turn(-90);
+}
+	rob.move(100);
+	rob.turn(45);
+	rob.move(45);
+	rob.turn(90);
+	rob.move(45);
+	rob.turn(45);
+	rob.move(100);
+	rob.turn(-90);
+	rob.setPenColor(0,255,0);
+	rob.move(20);
+	rob.setPenColor(0,0,0);
+	rob.turn(-90);
+	
+	rob.penDown();
+	rob.setSpeed(10000);
+	rob.move(100);
+	rob.turn(90);
+	rob.move(48);
+	rob.turn(90);
+	rob.move(100);	
+	rob.turn(-90);
+	rob.setPenColor(0,255,0);
+	rob.move(20);
+	rob.setPenColor(0,0,0);
+	rob.turn(-90);
+	
+	rob.penDown();
+	rob.setSpeed(10000);
+	rob.move(500);
+	rob.turn(90);
+	rob.move(48);
+	rob.turn(90);
+	rob.move(500);	
+	rob.turn(-90);
+	rob.setPenColor(0,255,0);
+	rob.move(20);
+	rob.setPenColor(0,0,0);
+	
+	for (int i = 0; i < 40; i++) {
+		rob.move(5);
+rob.turn(-5);
+	}
+	for (int i = 0; i < 25; i++) {
+	rob.turn(-15);
+rob.move(5);	
+	}
+rob.turn(-15);
+rob.move(1);
+rob.moveTo(150, 550);
+	
+	}
 	public void run() {
+		
 		
 	}
 }
