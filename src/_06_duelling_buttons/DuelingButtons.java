29a8828a0935f86ed56dd731ef3b306f70c0a5d2
root <root@b1af13a3252d>
2020-09-10 02:26:18 +0000

    DuelingButtons

--- src/_06_duelling_buttons/DuelingButtons.java
+++ src/_06_duelling_buttons/DuelingButtons.java
@@ -39,26 +39,40 @@ label.setText("click me.");
 		// 4. Set the text of the rightButton to "Click me!"
 label.setText("click me.");
 		// 5. Add an action listener to the leftButton
-
+	leftButton.addActionListener(null);
 		// 6. Add an action listener to the rightButton
-
+	rightButton.addActionListener(null);
 		// 7. Add the leftButton to the panel
 panel.add(leftButton);
 		// 8. Add the rightButton to the panel
 panel.add(rightButton);
 		// 9. Pack the frame
 frame.pack(); 
+
+
 //
 ///
 ////
 		// 10. Set the title of the frame to "Demanding Buttons"
+frame.setTitle("Demanding Buttons");
 
+ 
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		JButton buttonPressed = (JButton) arg0.getSource();
-
+		
+		if(leftButton == buttonPressed){
+			
+			rightButton.setText("NO CLICK ME!!!!!!!!!!!!!!!!");
+			
+			rightButton.setPreferredSize(BIG);
+			
+			leftButton.setText("click me !!!!!!!!!!");
+			
+			leftButton.setPreferredSize(SMALL);
+		}
 		/* If the buttonPressed was the leftButton.... */
 		// Set the text of the rightButton to "No, click Me!"
 		// Set the PREFERRED size of the rightButton to BIG
@@ -70,3 +84,4 @@ frame.pack();
 		frame.pack();
 	}
 }
+	
