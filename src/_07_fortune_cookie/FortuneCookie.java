8f8fdda7d661fb81f39a22f8aa59f5f04cef0828
root <root@40c35504f51f>
2020-10-22 02:27:49 +0000

    FortuneCookie

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -2,9 +2,11 @@ package _07_fortune_cookie;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class FortuneCookie implements ActionListener {
 
@@ -21,12 +23,67 @@ public class FortuneCookie implements ActionListener {
 
 		button.addActionListener(this); 
 		
+		JOptionPane.showMessageDialog(null, "WOOHOO!");
+		
+		
+		   
+		int rand = new Random().nextInt(5);
+		   
+		   JOptionPane.showMessageDialog(null, "Click ok to see your number fortune");
+		   
+		JOptionPane.showMessageDialog(null, "You got "+rand);
+		
+		
+		
+		
+		
+		
+		
+	
 		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
+		
+		int rand = new Random().nextInt(5);
+		
+		if (rand == 1 ) {
+			
+			System.out.println("Your fortune is INFINITE RICHES!");
+			
+		}
+		
+
+		if (rand == 2 ) {
+			
+			System.out.println("Your fortune is terrible, you will become POOR!");
+			
+		}
+		
+
+		if (rand == 3 ) {
+			
+			System.out.println("Your fortune is absolutely NOTHING!");
+			
+		}
+		
+
+		if (rand == 4 ) {
+			
+			System.out.println("Your fortune is that you will get into a terrible accident...");
+			
+		}
+		
+
+		if (rand == 5 ) {
+			
+			System.out.println("Your fortune is ");
+			
+		}
+		
+		
 		
 	}
 
